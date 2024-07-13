4879f4034791a586a5e374e6591dc1061a26d298
iSAD <bradx2713@gmail.com>
2024-07-13 14:13:56 +0200

    Initial Commit

---
+++ Animated/AnimatedApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedApp.swift
+//  Animated
+//
+//  Created by Tafadzwa Alexander on 2024/07/13.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
