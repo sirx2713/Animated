4c80252f7bdfb0c9e91193d2ba7d472b65e88beb
iSAD <bradx2713@gmail.com>
2024-07-13 14:35:07 +0200

    Learning Animated assets

---
+++ Animated/OnBoarding.swift
@@ -0,0 +1,18 @@
+//
+//  OnBoarding.swift
+//  Animated
+//
+//  Created by Tafadzwa Alexander on 2024/07/13.
+//
+
+import SwiftUI
+
+struct OnBoarding: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnBoarding()
+}
