b0c80986e60eff1f02c6bef01e3b290e3c6579b0
iSAD <bradx2713@gmail.com>
2024-07-13 16:37:48 +0200

    Leveling up!

--- Animated/OnBoarding.swift
+++ Animated/OnBoarding.swift
@@ -6,10 +6,42 @@
 //
 
 import SwiftUI
+import RiveRuntime
 
 struct OnBoarding: View {
+    
+    let button = RiveViewModel(fileName: "button")
+    @State var buttonStartFont = 30
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            RiveViewModel(fileName: "shapes").view()
+                .ignoresSafeArea()
+                .blur(radius: 30)
+                .background(
+                    Image("Spline")
+                        .blur(radius: 50)
+                        .offset(x: 200, y: 100)
+                )
+            button.view()
+                .frame(width: 236, height: 64)
+                .overlay(
+                    Label("Start", systemImage: "arrow.forward")
+                        .offset(x: 4, y: 4)
+                        .font(.system(size: CGFloat(buttonStartFont)))
+                )
+                .background(
+                    Color(.black)
+                        .cornerRadius(30)
+                        .blur(radius: 10)
+                        .opacity(0.3)
+                        .offset(y: 10)
+                )
+                .onTapGesture {
+                    button.play(animationName: "active")
+                    buttonStartFont = 24
+                }
+        }
     }
 }
 
